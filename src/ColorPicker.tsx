f663ce6b374a7cb67463ae868223d19baab11ca1
sungjong <mosj@useb.co.kr>
2022-10-13 20:07:38 +0900

    edit README.md & add feature color tint

--- src/ColorPicker.tsx
+++ src/ColorPicker.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import { State } from './ColorContainer';
 
 interface Props {
@@ -8,16 +8,30 @@ interface Props {
 }
 
 const ColorPicker = ({ index, state, handleChange }: Props) => {
+  const labelRef = useRef<HTMLLabelElement>(null);
+
   const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const { value } = e.target;
     console.log({ value });
     handleChange(value, index);
+    if (labelRef && labelRef.current) {
+      labelRef.current.style.backgroundColor = value;
+    }
   };
-
+  
+  useEffect(() => {
+    if (labelRef && labelRef.current) {
+      labelRef.current.style.backgroundColor = state.color;
+    }
+  }, [state.color])
+  
   return (
     <div className="color-picker">
       color picker
-      <input type="color" onChange={onChange} value={state.color}></input>
+      <div>
+        <input className="color-picker-input" id={`color-picker-${index}`} type="color" onChange={onChange} value={state.color}></input>
+        <label ref={labelRef} htmlFor={`color-picker-${index}`}></label>
+      </div>
     </div>
   );
 };
